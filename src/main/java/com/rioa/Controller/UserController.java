ca705bd1f3bc3170799b9f0545e12b7c0b07c672
rioa <rioaformyking@gmail.com>
2022-05-02 15:35:13 +0800

    fix and add : user's avatar controller

--- src/main/java/com/rioa/Controller/UserController.java
+++ src/main/java/com/rioa/Controller/UserController.java
@@ -11,8 +11,17 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.server.ResponseStatusException;
 
+import javax.imageio.IIOImage;
+import javax.imageio.ImageIO;
+import javax.imageio.ImageWriteParam;
+import javax.imageio.ImageWriter;
+import javax.imageio.stream.ImageOutputStream;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Arrays;
+import java.util.Iterator;
 
 @RestController
 @RequestMapping("/api/user")
@@ -35,10 +44,15 @@ public class UserController {
     public String postAvatar(@RequestParam("image") MultipartFile multipartFile,
                              Authentication authentication) throws IOException {
         User user = userRepository.findByUsername(authentication.getName()).get();
-        System.out.println(multipartFile.getContentType());
-        user.setAvatar(multipartFile.getBytes());
+//        System.out.println(multipartFile.getContentType());
+        if (!Arrays.asList("image/jpeg", "image/png").contains(multipartFile.getContentType())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Wrong file type");
+        }
+
+        byte[] bytes = compressImage(multipartFile);
+        user.setAvatar(bytes);
         userRepository.save(user);
-        return "success";
+        return user.getUsername() + "add avatar successfully";
     }
 
     //get user avatar
@@ -50,6 +64,48 @@ public class UserController {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Avatar not found");
         }
         return user.getAvatar();
+    }
+
+    public byte[] compressImage(MultipartFile image) throws IOException
+    {
+
+        InputStream inputStream = image.getInputStream();
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+
+        float imageQuality = 0.3f;
+
+        // Create the buffered image
+        BufferedImage bufferedImage = ImageIO.read(inputStream);
+
+        // Get image writers
+        Iterator<ImageWriter> imageWriters = ImageIO.getImageWritersByFormatName("jpg"); // Input your Format Name here
+
+        if (!imageWriters.hasNext())
+            throw new IllegalStateException("Writers Not Found!!");
+
+        ImageWriter imageWriter = imageWriters.next();
+        ImageOutputStream imageOutputStream = ImageIO.createImageOutputStream(outputStream);
+        imageWriter.setOutput(imageOutputStream);
+
+        ImageWriteParam imageWriteParam = imageWriter.getDefaultWriteParam();
+
+        // Set the compress quality metrics
+        imageWriteParam.setCompressionMode(ImageWriteParam.MODE_EXPLICIT);
+        imageWriteParam.setCompressionQuality(imageQuality);
+
+        // Compress and insert the image into the byte array.
+        imageWriter.write(null, new IIOImage(bufferedImage, null, null), imageWriteParam);
+
+        byte[] imageBytes = outputStream.toByteArray();
+
+        // close all streams
+        inputStream.close();
+        outputStream.close();
+        imageOutputStream.close();
+        imageWriter.dispose();
+
+
+        return imageBytes;
     }
 
 }
