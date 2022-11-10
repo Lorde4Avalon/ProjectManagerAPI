b84a4cc3b627008a0a999cf6333e0d0c673ed27d
coderwdd <coderwdd@gmail.com>
2022-11-10 17:31:14 +0800

    response data format

---
+++ src/main/java/com/rioa/Configurer/respond/DefaultExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.rioa.Configurer.respond;
+
+import com.murmur.murmur.config.respond.ResponseCode;
+import com.murmur.murmur.config.respond.ResponseData;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class DefaultExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler({ AuthenticationException.class })
+    @ResponseBody
+    public ResponseEntity<ResponseData> handleAuthenticationException(Exception ex) {
+
+        ResponseData re = new ResponseData(ResponseCode.UNAUTHORIZED.getCode(),
+          ResponseCode.UNAUTHORIZED.getMessage(), null);
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(re);
+    }
+}
---
+++ src/main/java/com/rioa/Configurer/respond/GlobalExceptionHandler.java
@@ -0,0 +1,37 @@
+package com.rioa.Configurer.respond;
+
+import com.murmur.murmur.config.respond.ResponseCode;
+import com.murmur.murmur.config.respond.ResponseData;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.web.servlet.error.BasicErrorController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Map;
+
+@RestControllerAdvice
+@RestController
+@Slf4j
+public class GlobalExceptionHandler {
+    @Autowired
+    private BasicErrorController basicErrorController;
+
+    @ExceptionHandler(Throwable.class)
+    public ResponseData handleException(Throwable t, HttpServletResponse response) throws Throwable {
+        return ResponseData.Companion.error(ResponseCode.FAILURE.getCode(), t.getMessage(), null);
+    }
+
+    @GetMapping("${server.error.path:${error.path:/error}}")
+    public ResponseData error(Throwable t, HttpServletRequest request, HttpServletResponse response) {
+        ResponseEntity<Map<String, Object>> error = basicErrorController.error(request);
+        Map<String, Object> body;
+        String notExistingPath = error == null ? null : ((body = error.getBody()) == null ? null : String.valueOf(body.get("path")));
+        return ResponseData.Companion.error(error.getStatusCodeValue(), error.getStatusCode().name(), null);
+    }
+}
